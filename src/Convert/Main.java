6e499ef85d83eb2c64fbd05f6eec1352fdbad3d7
Blackcourse <66360396+Blackcourse@users.noreply.github.com>
2022-05-28 11:31:04 +0300

    calc change

--- src/Convert/Main.java
+++ src/Convert/Main.java
@@ -14,16 +14,16 @@ public class Main {
             int value = scanner.nextInt();
             switch (c) {
                 case 1:
-                    System.out.println("Kilograms");
                     double kg = 1;
                     double lb = 2.2046 * kg;
+                    System.out.println("Result");
                     System.out.println("Pounds: " + value * lb);
                     System.out.println("Kilograms: " + value * kg);
                     break;
                 case 2:
-                    System.out.println("Pounds");
                     double kg1 =0.453592 ;
                     double lb1 =1/kg1 ;
+                    System.out.println("Result");
                     System.out.println("Kilograms: "+ value/lb1);
                     System.out.println("Pounds: " + value);
                     break;
@@ -36,16 +36,16 @@ public class Main {
             int value = scanner.nextInt();
             switch (c) {
                 case 1:
-                    System.out.println("Kilometer");
                     double miles =0.62137100000000000666 ;
                     double meter = 1*miles;
+                    System.out.println("Result");
                     System.out.println("Miles: " + value * meter);
                     System.out.println("Kilometer: " + value);
                     break;
                 case 2:
-                    System.out.println("Miles");
                     double meter1 =1.60934 ;
                     double miles1 = 1/meter1;
+                    System.out.println("Result");
                     System.out.println("Miles:" + value);
                     System.out.println("Kilometer: " + value/miles1);
                     break;
